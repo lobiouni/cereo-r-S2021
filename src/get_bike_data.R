5178ac2cb9fd684e504f696529e21eb2b769f920
lobiouni <lorena.biouni@gmail.com>
2022-07-21 14:34:14 -0300

    Add bike data.

---
+++ src/get_bike_data.R
@@ -0,0 +1,4 @@
+# Data for bike user records
+# Can't download by R, so we have to do it by hand.
+# Link to download: https://drive.google.com/file/d/1ccS-Bu581Nq_kO6Rb1YNtCt2qAb2MpRS/view?usp=sharing
+
